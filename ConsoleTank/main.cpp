2249f9ec1b574ea07c353726e38a17ff56a3786b
fyc <fyc@time-vr.com>
2020-01-13 15:22:31 +0800

    增加GameMode

--- ConsoleTank/main.cpp
+++ ConsoleTank/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+class GameMode
+{
+public:
+	bool Init()
+	{
+		return true;
+	}
+
+	void Tick()
+	{
+
+	}
+};
+
 int main()
 {
+	GameMode mode;
+	if (!mode.Init())
+	{
+		std::cout << "³õÊ¼»¯Ê§°Ü" << std::endl;
+		return -1;
+	}
+
+	mode.Tick();
+
 	system("pause");
 }
